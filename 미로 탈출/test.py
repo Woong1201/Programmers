62325478d0c08c10a68593ba7c1777ab5cdef3b4
woong <duddnd170@ajou.ac.kr>
2021-12-30 23:25:03 +0900

    2021 카카오 채용연계형 인턴십

--- 미로 탈출/test.py
+++ 미로 탈출/test.py
@@ -1,13 +1,37 @@
 def solution(n, start, end, roads, traps):
-    answer = 0
-    return answer
+    roads.sort()
+    step = []
+    
+    for i in range(len(roads)) :
+        if roads[i][0] == start:
+            while roads[i][0] == start:
+                step.append(roads[i][1])
+        break
+    
+    while True :
+        
+        
+        
+    
+    return 1
 
 
+def maze(path):
+    
+    
+    
+    
+    return 1
 
 
-
-
-
+import copy
 n = 4; start = 1; end = 4;
 roads = [[1, 2, 1], [3, 2, 1], [2, 4, 1]]
+path = [roads]
 traps = [2, 3]
+roads1 = copy.deepcopy(path)
+roads1[0][0][0] = 0
+roads
+roads1
+roads.sort()
+roads
