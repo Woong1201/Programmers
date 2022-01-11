81b3585db803f793e619e76ae02848cc9e874ffe
woong <duddnd170@ajou.ac.kr>
2022-01-11 15:43:11 +0900

    2020 카카오 인턴십

--- 수식 최대화/test.py
+++ 수식 최대화/test.py
@@ -1,41 +1,65 @@
 import itertools
-cal = list(itertools.permutations(['+','-','*'],3))
+per = list(itertools.permutations(['+','-','*'],3))
 
-def cal(a, b, c):
-    if c == '+':
-        return int(a) + int(b)
-    elif c == '-':
-        return int(a) - int(b)
-    else :
-        return int(a) * int(b)
+def cal(num, cal, sign):
+    num1 = num
+    cal1 = []
+    
+    for s in sign:
+        cal1 = []
+        i = 0
+        k = -1
         
+        for j in range(len(cal)):
+            if cal[j] == s:
+                num1 = num1[0:i] + [eval(str(num1[i]) + s + str(num1[i+1]))] + num1[i+2:]
+                cal1 += cal[k+1:j]
+                k = j
+                i-=1
+            i+=1
+
+        cal1 += cal[k+1:]
+        cal = cal1
+
+    
+    return abs(num1[0])
+
 def solution(expression):
-    num = []
-    cal = []
     answer = []
+    num=[]
+    c=[]
     
     i = 0
     for j in range (len(expression)):
         if expression[j].isdigit() == False:
             num.append(expression[i:j])
-            cal.append(expression[j])
+            c.append(expression[j])
             i = j + 1
     num.append(expression[i:])
     
-    num1 = []
-    for i in range(len(cal)):
-        if i == '+':
+    for p in per:
+        answer.append(cal(num,c,p))
             
             
-    return cal
+    return max(answer)
 
 
 expression = "100-200*300-500+20"
-
 solution(expression)
 
-import re
-x=re.split('-', expression)
-x
-int('3+4')
 
+
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            print(temp)
+            temp_list.append(f'({b.join(temp)})')
+            print(temp_list)
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
