b08894a44e73ad3923f825dcf702137427f93e52
woong <duddnd170@ajou.ac.kr>
2022-01-31 23:22:59 +0900

    2021 카카오 채용연계형 인턴십

--- 표 편집/test5.py
+++ 표 편집/test5.py
@@ -1,37 +1,4 @@
-def solution1(n, k, cmd):
-    answer = ['O'] * n
-    hist = []
-    p = 0
-    for i in cmd:
-        if i[0] == 'D':
-            p += int(i[2:])
-        elif i[0] == 'U':
-            p -= int(i[2:])
-        else :
-            while p > 0:
-                k += 1
-                if answer[k] == 'O':
-                    p -= 1
-            while p < 0:
-                k -= 1
-                if answer[k] == 'O':
-                    p += 1
-            if i[0] == 'C':
-                answer[k] = 'X'
-                hist.append(k)
-                k2 = k - 1
-                while k < n and answer[k] == 'X':
-                    k += 1
-                if k == n:
-                    k = k2
-                    while answer[k] == 'X':
-                        k -= 1
-            else :
-                answer[hist.pop()] = 'O'
-
-    return ''.join(answer)
-
-def solution2(n, k, cmd):
+def solution(n, k, cmd):
     answer = {}
     for i in range(-1, n + 1):
         answer[i] = ['O', i - 1, i + 1]
@@ -74,13 +41,6 @@ def solution2(n, k, cmd):
     ans = [answer[i][0] for i in range(n)]
     return ''.join(ans)
 
-def solution(n, k, cmd):
-    if n > 300000:
-        return solution1(n, k, cmd) #효율성 8 실패
-    else :
-        return solution2(n, k, cmd) #효율성 6 실패
-    #test6_n > 300000 > test8_n > 200000
-
 n = 8; k = 2
 cmd1 = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
 cmd2 = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
@@ -88,3 +48,5 @@ print(solution(n, k, cmd1))
 print(solution(n, k, cmd2))
 sol1 = "OOOOXOOO"
 sol2 = "OOXOXOOO"
+
+#효율성 6 실패
