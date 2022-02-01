32cdc7aa2ce588a63ab9e435ac53fe6354b7d687
woong <duddnd170@ajou.ac.kr>
2022-02-01 11:15:20 +0900

    2020 카카오 인턴십

--- 경주로 건설/test.py
+++ 경주로 건설/test.py
@@ -13,67 +13,53 @@ def direction(loc):
 
 def solution(board):
     n = len(board) - 1
-    route = [[(1,0),(0,0)], [(0,1),(0,0)]]
+    route = [[(1,0),(0,0),100,1], [(0,1),(0,0),100,1]]
     answer = 0
     if board[0][1] == 1:
         dic = {(0,0) : 0, (1,0) : 100}
-        route.remove([(0,1),(0,0)])
+        route.remove([(0,1),(0,0),100,1])
     elif board[1][0] == 1:
         dic = {(0,0) : 0, (0,1) : 100}
-        route.remove([(1,0),(0,0)])
+        route.remove([(1,0),(0,0),100,1])
     else :
         dic = {(0,0) : 0, (1,0) : 100, (0,1) : 100}
     
-    while len(route)!=0:
+    while route:
         loc = route[0]
-        if answer!=0 and dic[loc[0]] > answer:
+        if answer!=0 and loc[2] >= answer:
             del(route[0])
             continue
         
-        direct = direction(loc)
+        direct = direction(loc[0:2])
         d0 = direct[0]
         d1 = direct[1]
         d2 = direct[2]
         
-        if 0<=d0[0][0]<=n and 0<=d0[0][1]<=n\
-            and board[d0[0][0]][d0[0][1]]==0:
-                if dic.get(d0[0],-1)==-1 :
-                    dic[d0[0]] = dic[d0[1]]+100
-                    route.append(d0)
-                    
-                else:
-                    if dic[d0[0]]>=dic[d0[1]]+100:
-                        dic[d0[0]] = dic[d0[1]]+100
-                        route.append(d0)
-                    elif dic[d0[1]]-400<=dic[d0[0]]:
-                        d00=direction(d0)[0]
-                        if 0<=d00[0][0]<=n and 0<=d00[0][1]<=n\
-                            and board[d00[0][0]][d00[0][1]]==0\
-                                and dic[d00[0]>=dic[d0[1]]+200]:
-                                dic[d00[0]] = dic[d0[1]]+200
-                                route.append(d00)
-                    
-        if 0<=d1[0][0]<=n and 0<=d1[0][1]<=n\
-            and board[d1[0][0]][d1[0][1]]==0:
-                if dic.get(d1[0],-1)==-1 or dic[d1[0]]>=dic[d1[1]]+600:
-                    dic[d1[0]] = dic[d1[1]]+600
-                    route.append(d1)
-                    
-        if 0<=d2[0][0]<=n and 0<=d2[0][1]<=n\
-            and board[d2[0][0]][d2[0][1]]==0:
-                if dic.get(d2[0],-1)==-1 or dic[d2[0]]>=dic[d2[1]]+600:
-                    dic[d2[0]] = dic[d2[1]]+600
-                    route.append(d2)
+        if 0<=d0[0][0]<=n and 0<=d0[0][1]<=n and board[d0[0][0]][d0[0][1]]==0:
+                if dic.get(d0[0],-1)==-1 or dic[d0[0]] >= loc[2] + 100:
+                    dic[d0[0]] = loc[2] + 100
+                    route.append(d0+[loc[2]+100, 1])
+                elif loc[3] == 1:
+                    route.append(d0+[loc[2]+100, 0])
+
+        if 0<=d1[0][0]<=n and 0<=d1[0][1]<=n and board[d1[0][0]][d1[0][1]]==0:
+                if dic.get(d1[0],-1)==-1 or dic[d1[0]] >= loc[2] + 600:
+                    dic[d1[0]] = loc[2] + 600
+                    route.append(d1+[loc[2]+600,1])
+                elif loc[3] == 1:
+                    route.append(d1+[loc[2]+600,0])
+
+        if 0<=d2[0][0]<=n and 0<=d2[0][1]<=n and board[d2[0][0]][d2[0][1]]==0:
+                if dic.get(d2[0],-1)==-1 or dic[d2[0]]>= loc[2] + 600:
+                    dic[d2[0]] = loc[2] + 600
+                    route.append(d2+[loc[2]+600,1])
+                elif loc[3] == 1:
+                    route.append(d2+[loc[2]+600,0])
         
         answer = dic.get((n,n),0)
         del(route[0])
     return answer
 
-
-            
-        
-        
-      
 board = [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]
-#board = [[0,0,0],[0,0,0],[0,0,0]]
-solution(board)
+print(solution(board))
+answer = 3800
